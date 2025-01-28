43efa750004b2b09fdf8074193c65722ac22fd5c
Raul Rodrigues <91432196+RaulRJ5@users.noreply.github.com>
2025-01-28 13:15:53 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,46 @@
 import Estruturas.MatrizEsparsa;
 import Estruturas.MatrizEstatica;
+import Estruturas.MatrizListas;
 
 public class Main {
 
-	public static void main(String[] args) {
-		MatrizEstatica m = new MatrizEstatica(3);
-		m.insereElem(0, 0, 1);
-		m.insereElem(0, 1, 2);
-		m.insereElem(1, 0, 3);
-		m.insereElem(1, 1, 4);
-		m.imprime();
-		
-		m.insereElem(0, 0, 1);
-		m.removeElem(0, 1);
-		m.imprime();
-		m.insereElem(0, 1, 2);
-		
-		m.imprime();
-		
-		MatrizEstatica m2 = new MatrizEstatica(3);
-		m2.insereElem(0, 0, 2);
-		m2.insereElem(0, 1, 5);
-		m2.insereElem(1, 0, 6);
-		m2.insereElem(1, 1, 3);
-		
-		MatrizEstatica m3 = new MatrizEstatica(3);
-		m3.insereElem(0, 0, 4);
-		m3.insereElem(0, 1, 3);
-		m3.insereElem(0, 2, 1);
-		m3.insereElem(1, 0, 0);
-		m3.insereElem(1, 1, 2);
-		m3.insereElem(1, 2, 5);
-		m3.insereElem(2, 0, 0);
-		m3.insereElem(2, 1, 0);
-		m3.insereElem(2, 2, 1);
-		
-		System.out.println("Matriz 3x3");
-		m3.imprime();
-		
-		if(m3.ehMatrizTriangSup()) {
-			System.out.println("É uma matriz Triangular Superior");
-		}
-		else {
-			System.out.println("Não é uma matriz Triangular Superior\n");
-		}
-		
-		System.out.println("Matriz");
-		m.imprime();
-		
-		if(m.ehDiagonal()) {
-			System.out.println("Matriz Diagonal");
-		}
-		else {
-			System.out.println("A Matriz não é diagonal\n");
-		}
-		
-		
-		System.out.println("Matriz Transposta");
-		MatrizEsparsa transp = m.obtemTransposta();
-		transp.imprime();
+		public static void main(String[] m3) {
+		MatrizListas m2 = new MatrizListas(3);
+		MatrizListas m33 = new MatrizListas(3);
+		MatrizListas m4 = new MatrizListas(3);
 
-		System.out.println("Matriz m");
-		m.imprime();
-		System.out.println("Segunda Matriz");
-		m2.imprime();
+			m2.insereElem(0, 0, 1);
+			m2.insereElem(1, 0, 2);
+			m2.insereElem(1, 1, 3);
+			m2.insereElem(2, 0, 4);
+			m2.insereElem(2, 1, 5);
+			m2.insereElem(2, 2, 6);
+
+
+			m33.insereElem(0, 0, 1);
+			m33.insereElem(0, 1, 2);
+			m33.insereElem(1, 0, 4);
+			m33.insereElem(0, 2, 3);
+			m33.insereElem(1, 1, 4);
+			m33.insereElem(1, 2, 5);
+			m33.insereElem(2, 2, 6);
+
+			m4.insereElem(0, 0, 1);
+			m4.insereElem(0, 1, 2);
+			m4.insereElem(0, 2, 3);
+			m4.insereElem(1, 0, 2);
+			m4.insereElem(1, 1, 4);
+			m4.insereElem(1, 2, 5);
+			m4.insereElem(2, 0, 3);
+			m4.insereElem(2, 1, 5);
+			m4.insereElem(2, 2, 6);
+			if(m4.ehSimetrica()){
+				System.out.println("É simetricaa");
+			}
+			else
+				System.out.println("naoooo");
 		
-		System.out.println("Soma entre as matrizes");
-		MatrizEstatica soma = m.somaMatriz(m2);
-		soma.imprime();
-		
-		
-		System.out.println("Multiplicação entre as matrizes");
-		MatrizEstatica mult = m.multiplicaMatriz(m2);
-		mult.imprime();
+
 		
 	}
 
